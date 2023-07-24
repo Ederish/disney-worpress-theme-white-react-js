6bdbb1abd509d3ea727d8724a87e50b9ac93eae4
Ederish <63983547+Ederish@users.noreply.github.com>
2023-07-23 18:50:04 -0600

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./../Navbar";
+import UserSelector from "./../UserSelector";
+import "./Header.css";
+
+const Header = ({ className }) => {
+  return (
+    <header className={className}>
+      <Navbar />
+      <UserSelector />
+    </header>
+  );
+};
+export default Header;
